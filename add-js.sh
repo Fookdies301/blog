17491726b2227b5bc592a4fb1f867b397072af37
Fookdies301 <pengfkun@gmail.com>
2019-01-06 02:11:41 +0800

    添加了几篇文章

---
+++ add-js.sh
@@ -0,0 +1,9 @@
+echo "++++++++++ 开始执行{hexo clean}命令 ++++++++++"
+hexo clean
+
+echo "++++++++++++ 开始执行{hexo g}命令 ++++++++++++"
+hexo g
+
+echo "++++++++++++ 开始执行{hexo d}命令 ++++++++++++"
+hexo d 2>/dev/null
+
---
+++ source/categories/index.md
@@ -0,0 +1,5 @@
+﻿---
+title: categories
+date: 2019-01-05 01:21:49
+type: "categories"
+---
